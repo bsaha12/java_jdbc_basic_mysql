0e4949f6cc198487aabc8c73f73831e98091d86d
Bhaskar Saha <bhaskar12jan678@gmail.com>
2024-03-04 13:24:28 +0530

    createdb deletedb createtable methods added

--- src/App.java
+++ src/App.java
@@ -1,9 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Database db = new Database("Student") ;
+        Database db = new Database("Students") ;
 
-        // first creating a database .. just one time
+        // creating a database .. just one time
         // db.createDatabase();
+
+        // deleting database if needed 
+        // db.deleteDatabase();
+
+        // create table once
+        db.createTable("student");
+
     }
 }
--- src/Database.java
+++ src/Database.java
@@ -8,16 +8,50 @@ public class Database {
     private String username;
     private String password;
 
-    Database(String name) {
-        dbName = name;
-        url = "jdbc:mysql://localhost:3306/";
+    Database(String dbName) {
+        this.dbName = dbName;
+        url = "jdbc:mysql://localhost:3306/" + dbName;
         username = "root";
         password = "123456789";
     }///
 
+    public String getDbName() {
+        return dbName;
+    }
+
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // creating your own DB
     public void createDatabase() {
         try {
-            Connection con = DriverManager.getConnection(url, username, password);
+            String base_url = "jdbc:mysql://localhost:3306/";
+            Connection con = DriverManager.getConnection(base_url, username, password);
             Statement st = con.createStatement();
 
             String query = "create database " + dbName + ";";
@@ -25,8 +59,43 @@ public class Database {
             st.execute(query);
 
             con.close();
+
             System.out.println(dbName + " Created Successfully");
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // creating table in a particular DB
+    public void deleteDatabase() {
+        try {
+            String query = "drop database " + dbName + ";";
+
+            Connection con = DriverManager.getConnection(url, username, password);
+            Statement st = con.createStatement();
+
+            st.execute(query);
+            con.close();
+
+            System.out.println(dbName + " Deleted Successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }///
+
+    public void createTable(String name) {
+        String query = "create table " + name + " (id int , name varchar(50) , marks int);";
+        try {
+            Connection con = DriverManager.getConnection(url, username, password);
+            Statement st = con.createStatement();
+
+            st.execute(query);
+
+            con.close();
+
+            System.out.println(name + " Table created successfully");
         } catch (Exception e) {
             e.printStackTrace();
         }
